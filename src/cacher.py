32d5c6f8b72a519d860d505c304a443352d8b9ae
Saša Tomić <sasa.git@kalaj.org>
2024-09-30 22:47:11 +0200

    Use strings in the cache, instead of lists

--- src/cacher.py
+++ src/cacher.py
@@ -1,39 +1,37 @@
 import pathlib
-import json
 import diskcache as dc
 
 
 class Cacher:
     def __init__(self):
-        self.cache = dc.Cache(pathlib.Path(__file__).parent / "cache")
+        self.cache = dc.Cache(pathlib.Path(__file__).parent.parent / "cache")
 
     def cache_get_unit(self, unit: dict):
-        unit_cache_key = json.dumps(unit["source"])
-        unit_cache_value = self.cache.get(unit_cache_key)
-        if unit_cache_value:
-            print("Found cache %s --> %s" % (unit_cache_key, unit_cache_value))
-            return json.loads(unit_cache_value)
-        return None
+        """
+        Check if all translations are in cache. If so, return translations.
+        """
+        translations = []
+        for source_string in unit["source"]:
+            translation = self.cache_get_string(source_string)
+            if translation is None:
+                return None
+            translations.append(translation)
+        return translations
 
     def cache_update_unit(self, unit: dict):
-        unit_cache_key = json.dumps(unit["source"])
-        unit_cache_value = json.dumps(unit["target"])
-        print("Updating cache %s --> %s" % (unit_cache_key, unit_cache_value))
-        self.cache[unit_cache_key] = unit_cache_value
+        for s, t in zip(unit["source"], unit["target"]):
+            self.cache_update_string(s, t)
 
-    def cache_get_string(self, string: str):
-        string_cache_key = json.dumps([string])  # unit expects a list as key/value
-        string_cache_value = self.cache.get(string_cache_key)
-        if string_cache_value:
-            print("Found cache %s --> %s" % (string_cache_key, string_cache_value))
-            return json.loads(string_cache_value)[0]
-        return None
+    def cache_get_string(self, key: str):
+        value = self.cache.get(key)
+        if value is None:
+            return None
+        # print("Found cache %s --> %s" % (key, value))
+        return value
 
-    def cache_update_string(self, string: str):
-        string_cache_key = json.dumps([string])  # unit expects a list as key
-        string_cache_value = json.dumps([string])  # unit expects a list as value
-        print("Updating cache %s --> %s" % (string_cache_key, string_cache_value))
-        self.cache[string_cache_key] = string_cache_value
+    def cache_update_string(self, key: str, value: str):
+        # print("Updating cache %s --> %s" % (key, value))
+        self.cache[key] = value
 
     def cache_clear(self):
         self.cache.clear()
