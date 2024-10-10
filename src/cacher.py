c0bd7dc9dc77b5002244c488d8222f2a53bfebea
Saša Tomić <sasa.git@kalaj.org>
2024-10-10 19:05:10 +0200

    Match the case of the cached translation to the source string

--- src/cacher.py
+++ src/cacher.py
@@ -1,3 +1,4 @@
+import itertools
 import pathlib
 import diskcache as dc
 
@@ -12,8 +13,8 @@ class Cacher:
         """
         translations = []
         for source_string in unit["source"]:
-            translation = self.cache_get_string(source_string) or self.cache_get_string(
-                source_string.lower()
+            translation = self.cache_get_string(source_string) or match_complex_case(
+                source_string, self.cache_get_string(source_string.lower())
             )
             if translation is None:
                 return None
@@ -37,3 +38,22 @@ class Cacher:
 
     def cache_clear(self):
         self.cache.clear()
+
+
+def match_complex_case(reference_str, target_str):
+    if not reference_str or not target_str:
+        return None
+
+    def match_char_case(ref_char, target_char):
+        if ref_char.isupper():
+            return target_char.upper()
+        else:
+            return target_char.lower()
+
+    # Iterate over both strings and apply case based on the reference string
+    result = "".join(
+        match_char_case(ref_char, target_char)
+        for ref_char, target_char in zip(reference_str, itertools.cycle(target_str))
+    )
+
+    return result
