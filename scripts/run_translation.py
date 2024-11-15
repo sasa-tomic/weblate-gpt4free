2421d12ba59d5f6a829e8e700274e191cfbbcae6
Saša Tomić <sasa.git@kalaj.org>
2024-11-15 20:50:08 +0100

    support multiple weblate projects in the config.yml

--- scripts/run_translation.py
+++ scripts/run_translation.py
@@ -12,35 +12,37 @@ from src.cacher import Cacher
 def main():
     config = load_config("config/config.yml")
 
-    target_lang = config["weblate"]["target_language"]
-    cacher = Cacher(lang=target_lang)
-    gpt_translator = GPTTranslator(
-        prompt=config["gpt"]["prompt"],
-        prompt_extension_previous_translation=config["gpt"].get(
-            "prompt_extension_previous_translation"
-        ),
-        prompt_extension_flags_max_length=config["gpt"].get(
-            "prompt_extension_flags_max_length"
-        ),
-        prompt_glossary=config["gpt"].get("prompt_glossary"),
-        prompt_plural=config["gpt"].get("prompt_plural"),
-        prompt_remind_translate=config["gpt"].get("prompt_remind_translate"),
-        target_lang=target_lang,
-        api_key_expensive=config["gpt"].get("api_key_expensive"),
-        api_key_cheap=config["gpt"].get("api_key_cheap"),
-        cacher=cacher,
-    )
-    processor = TranslationProcessor(
-        api_url=config["weblate"]["api_url"],
-        projects=config["weblate"]["projects"],
-        target_lang=target_lang,
-        weblate_api_key=config["weblate"]["api_key"],
-        gpt_translator=gpt_translator,
-        cacher=cacher,
-    )
+    for weblate in config["weblate"]:
+        print(f"Processing {weblate['name']}...")
+        target_lang = weblate["target_language"]
+        cacher = Cacher(lang=target_lang)
+        gpt_translator = GPTTranslator(
+            prompt=config["gpt"]["prompt"],
+            prompt_extension_previous_translation=config["gpt"].get(
+                "prompt_extension_previous_translation"
+            ),
+            prompt_extension_flags_max_length=config["gpt"].get(
+                "prompt_extension_flags_max_length"
+            ),
+            prompt_glossary=config["gpt"].get("prompt_glossary"),
+            prompt_plural=config["gpt"].get("prompt_plural"),
+            prompt_remind_translate=config["gpt"].get("prompt_remind_translate"),
+            target_lang=target_lang,
+            api_key_expensive=config["gpt"].get("api_key_expensive"),
+            api_key_cheap=config["gpt"].get("api_key_cheap"),
+            cacher=cacher,
+        )
+        processor = TranslationProcessor(
+            api_url=weblate["api_url"],
+            projects=weblate["projects"],
+            target_lang=target_lang,
+            weblate_api_key=weblate["api_key"],
+            gpt_translator=gpt_translator,
+            cacher=cacher,
+        )
 
-    print("Processing incomplete translations...")
-    processor.process_incomplete_translations()
+        print("Processing incomplete translations...")
+        processor.process_incomplete_translations()
 
     print("Translation process completed.")
 
--- src/utils.py
+++ src/utils.py
@@ -2,5 +2,5 @@ import yaml
 
 
 def load_config(config_path):
-    with open(config_path, "r") as config_file:
+    with open(config_path, encoding="utf-8") as config_file:
         return yaml.safe_load(config_file)
